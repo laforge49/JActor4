c33c115c4b87191c5f8447e7ec50219eb9be8e1c
Bill La Forge <laforge49@gmail.com>
2014-06-18 10:35:17 -0400

    playing with lombok

---
+++ src/main/java/org/agilewiki/jactor4/One.java
@@ -0,0 +1,7 @@
+package org.agilewiki.jactor4;
+
+public class One {
+    public static void main(String[] args) {
+		System.out.println("Hi!");
+	}
+}
---
+++ src/test/java/org/agilewiki/jactor4/OneTest.java
@@ -0,0 +1,9 @@
+package org.agilewiki.jactor4;
+
+import junit.framework.TestCase;
+
+public class OneTest extends TestCase {
+    public void test1() {
+		System.out.println("Hi!");
+	}
+}
